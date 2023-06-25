bc06d0f0c5988149764102d201fa2ef7fa2e965e
7rebux <58339645+7rebux@users.noreply.github.com>
2023-06-25 09:24:17 +0200

    :sparkles: Add runner, tests & example day

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "1.8.20"
 }
 
-group = "org.example"
+group = "de.nosswald"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -18,5 +18,5 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(11)
+    jvmToolchain(8)
 }
